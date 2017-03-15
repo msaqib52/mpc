887d4a73c7d0952fd0c7fc4c85598c2c9c8038a3
Saqib <msaqib52@gmail.com>
2017-03-15 19:52:36 +0500

    exclude current_user from search results

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   # before_action :authenticate_user!, except: [:index]
 
   def index
-    @q = User.ransack(params[:q])
+    @q = User.where.not(id: current_user).ransack(params[:q])
     if params[:q] && params[:q][:platform_eq].present? && params[:q][:games_id_eq].present?
       @users = @q.result(distinct: true)
     end
